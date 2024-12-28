e431750aa879950207ae4a0c014d1032b7c9833c
Karthik-Kalburgi <kalburgikarthik0105@gmail.com>
2024-12-28 12:57:56 +0530

    Done with FrontPage

--- src/Components/StartGame.jsx
+++ src/Components/StartGame.jsx
@@ -1,15 +1,26 @@
-import React from 'react'
+import React from "react";
 
 const StartGame = () => {
   return (
-    
-    <div className='box-border border-2 border-black p-[18%] ] mt-[7%] ml-[12%] mr-[12%] '>
-    <div>
-        
-    </div>
-    </div>
-  )
-}
+    <div className="overscroll-none">
+      <div className="box-border border-2 border-black p-[7%] ] mt-[7%] ml-[12%] mr-[12%]">
+        <div className="grid gap-2  grid-cols-2">
+          <div>
+            <img className="h-50" src="images/Dice.png" alt="" srcset="" />
+          </div>
+          <div>
+            <h1 className="text-center font-serif font-bold text-3xl ">
+              DICE GAME
+            </h1>
 
-export default StartGame
+            <button className="ml-[55%] mt-[14%] bg-black text-white p-3 w-[65%] rounded-lg">
+              PlayNow
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
+export default StartGame;
