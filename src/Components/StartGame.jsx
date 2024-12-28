7eec6971308cd915ba886cf399ace78d29b2169c
Karthik-Kalburgi <kalburgikarthik0105@gmail.com>
2024-12-28 12:31:03 +0530

    Project Setup

---
+++ src/Components/StartGame.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const StartGame = () => {
+  return (
+    
+    <div className='box-border border-2 border-black p-[18%] ] mt-[7%] ml-[12%] mr-[12%] '>
+    <div>
+        
+    </div>
+    </div>
+  )
+}
+
+export default StartGame
+
